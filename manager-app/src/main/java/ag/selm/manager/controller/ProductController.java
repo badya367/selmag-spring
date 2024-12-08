c6a12d87a9e6fcb9172834c634a6b4497460143c
badya367 <badikov_dima@mail.ru>
2024-12-08 22:28:55 +0300

    Разделение приложения на Web приложение и RestApi. Создан новый модуль catalogue-service. Перенос сервиса и репозитория из модуля manager-app в catalogue-service. Созданы два Rest controller для списка товаров и для отдельного товара. Создан обработчик исключений для контроллеров. Протестил RestApi через Postman. Реализован клиента RestApi на стороне модуля manager-app с помощью RestClient. Рефакторинг кода в модуле manager-app. Зарегестрирован компонент в контексте приложения. Удалена валидация из manager-app, валидация интегрирована в catalogue-service.

--- manager-app/src/main/java/ag/selm/manager/controller/ProductController.java
+++ manager-app/src/main/java/ag/selm/manager/controller/ProductController.java
@@ -1,17 +1,15 @@
 package ag.selm.manager.controller;
 
+import ag.selm.manager.client.BadRequestException;
+import ag.selm.manager.client.ProductsRestClient;
 import ag.selm.manager.controller.payload.UpdateProductPayload;
 import ag.selm.manager.entity.Product;
-import ag.selm.manager.service.ProductService;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
@@ -21,51 +19,50 @@ import java.util.NoSuchElementException;
 @RequestMapping("catalogue/products/{productId:\\d+}")
 @RequiredArgsConstructor
 public class ProductController {
-    private final ProductService productService;
+    private final ProductsRestClient productsRestClient;
 
     private final MessageSource messageSource;
 
     @ModelAttribute("product")
     public Product product(@PathVariable("productId") int productId) {
-        return this.productService.findProduct(productId)
+        return this.productsRestClient.findProduct(productId)
                 .orElseThrow(() -> new NoSuchElementException("catalogue.errors.product.not_found"));
     }
+
     @GetMapping
-    public String getProduct(){
+    public String getProduct() {
         return "catalogue/products/product";
     }
 
     @GetMapping("edit")
-    public String getProductEditPage(){
+    public String getProductEditPage() {
         return "catalogue/products/edit";
     }
 
     @PostMapping("edit")
     public String updateProduct(@ModelAttribute(name = "product", binding = false) Product product,
-                                @Valid UpdateProductPayload payload,
-                                BindingResult bindingResult,
-                                Model model){
-        if(bindingResult.hasErrors()) {
+                                UpdateProductPayload payload,
+                                Model model) {
+        try {
+            this.productsRestClient.updateProduct(product.id(), payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.id());
+        } catch (BadRequestException exception) {
+
             model.addAttribute("payload", payload);
-            model.addAttribute("errors", bindingResult.getAllErrors().stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .toList());
+            model.addAttribute("errors", exception.getErrors());
             return "catalogue/products/edit";
-        } else {
-            this.productService.updateProduct(product.getId(), payload.title(), payload.details());
-            return "redirect:/catalogue/products/%d".formatted(product.getId());
         }
     }
 
     @PostMapping("delete")
     public String deleteProduct(@ModelAttribute("product") Product product) {
-        this.productService.deleteProduct(product.getId());
+        this.productsRestClient.deleteProduct(product.id());
         return "redirect:/catalogue/products/list";
     }
 
     @ExceptionHandler(NoSuchElementException.class)
     public String handleNoSuchElementException(NoSuchElementException exception, Model model,
-                                               HttpServletResponse response, Locale locale){
+                                               HttpServletResponse response, Locale locale) {
         response.setStatus(HttpStatus.NOT_FOUND.value());
         model.addAttribute("error",
                 this.messageSource.getMessage(exception.getMessage(), new Object[0],
