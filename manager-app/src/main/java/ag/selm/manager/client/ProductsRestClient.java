a43e16dd18e8153fef50fc4bd6be5f8b1be89251
badya367 <badikov_dima@mail.ru>
2024-12-16 00:50:38 +0300

    Подключение БД к проекту Подключение новых зависимостей Описание структуры БД Маппинг сущности в коде на сущность в БД (@Entity) Создание @Repository Настройка подключения к БД Баг-фикс изменения товара(update в БД) Создание пользовательского именованного запроса (@NamedQueries) Добавление возможности фильтрации списка товаров на UI

--- manager-app/src/main/java/ag/selm/manager/client/ProductsRestClient.java
+++ manager-app/src/main/java/ag/selm/manager/client/ProductsRestClient.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductsRestClient {
-    List<Product> findAllProducts();
+    List<Product> findAllProducts(String filter);
     Product createProduct(String title, String details);
     Optional<Product> findProduct(int productId);
     void updateProduct(int productId, String title, String details);
